c9225f1e5ef41032d2352d01514c205ae1ab82a0
Angel Teruel <atergal2706@g.educaand.es>
2026-09-24 19:26:55 +0200

    ejercicio uno probando dart

---
+++ Relacion 1/1hola-mundo.dart
@@ -0,0 +1,12 @@
+/* Ejercicio 1 de la relacion 1
+    1hola-mundo.dart
+    24/09/2026
+    autor: Ángel Teruel Gálvez */
+
+void main() {
+
+    String mensaje = 'Ángel';
+    int veces = 1;
+    print('Hola $mensaje');
+    print ('has ejecutado el programa ${veces*2-1} vez(es)');
+}
